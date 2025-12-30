4c35e95ca7c36905b9697fe653e9e2f2460a61f4
telekolia <telenickolai@gmail.com>
2025-12-30 09:53:32 +0300

    task_4

---
+++ lab_3-4/lab_python_fp/sorted.py
@@ -0,0 +1,10 @@
+from math import fabs
+
+data = [4, -30, 100, -100, 123, 1, 0, -1, -4]
+
+if __name__ == '__main__':
+    result = sorted(data, key=abs, reverse=True)
+    print(result)
+
+    result_with_lambda = sorted(data, key=lambda date_el: fabs(date_el), reverse=True)
+    print(result_with_lambda)
