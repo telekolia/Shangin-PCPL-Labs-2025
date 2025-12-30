bd52450bde65a77f7e11d30af3ca1fccd593ac2a
telekolia <telenickolai@gmail.com>
2025-12-30 09:41:12 +0300

    Third task

---
+++ lab_3-4/lab_python_fp/unique.py
@@ -0,0 +1,29 @@
+# Итератор для удаления дубликатов
+class Unique(object):
+    def __init__(self, items, **kwargs):
+        # Нужно реализовать конструктор
+        # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
+        # в зависимости от значения которого будут считаться одинаковыми строки в разном регистре
+        # Например: ignore_case = True, Aбв и АБВ - разные строки
+        #           ignore_case = False, Aбв и АБВ - одинаковые строки, одна из которых удалится
+        # По-умолчанию ignore_case = False
+        self.unique_items = []
+        for item in items:
+            compare_item = item
+            if "ignore_case" in kwargs and isinstance(item, str):
+                if kwargs["ignore_case"]:
+                    compare_item = item.lower()
+            if compare_item not in self.unique_items:
+                self.unique_items.append(compare_item)
+        self.current_index = 0
+
+    def __next__(self):
+        if self.current_index <= len(self.unique_items):
+            if self.current_index < len(self.unique_items):
+                self.current_index += 1
+            return self.unique_items[self.current_index - 1]
+        else:
+            print("Достигнут конец массива")
+
+    def __iter__(self):
+        return self
